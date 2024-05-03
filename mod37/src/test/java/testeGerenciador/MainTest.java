9968ab36219942c6dcdcf5e7e90f28f04ce5687e
ealande <erickalan23@gmail.com>
2024-05-02 22:26:21 -0300

    mod37

---
+++ mod37/src/test/java/testeGerenciador/MainTest.java
@@ -0,0 +1,14 @@
+package testeGerenciador;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    @DisplayName("Teste simples")
+    void test() {
+        assertTrue(true);
+    }
+}
