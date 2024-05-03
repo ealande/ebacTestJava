effa88395b01bb6404ea90e6c9d2bac27b1021bd
ealande <erickalan23@gmail.com>
2024-05-02 22:32:54 -0300

    mod40

---
+++ mod40/src/main/java/br/com/ebac/mod40/Mod40Application.java
@@ -0,0 +1,13 @@
+package br.com.ebac.mod40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod40Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mod40Application.class, args);
+    }
+
+}
---
+++ mod40/src/main/java/br/com/ebac/mod40/controller/ExemploController.java
@@ -0,0 +1,16 @@
+package br.com.ebac.mod40.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ExemploController {
+
+    @GetMapping
+    private ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
